ce371aa0f3f8621f722c651aed4541f8402f5499
_michdev <gamedev.mich@gmail.com>
2025-09-05 14:38:37 -0400

    fixed some positioning stuff

--- addons/dependency_graph/graph.gd
+++ addons/dependency_graph/graph.gd
@@ -2,19 +2,18 @@
 extends GraphEdit
 
 var interface: EditorInterface
-var nodes: Dictionary[String, GraphNode]
+var nodes: Dictionary[String, Control]
 
 const GRAPH_NODE = preload("res://addons/dependency_graph/graph_node.tscn")
 
 func _ready() -> void:
-	if !interface: return
 	add_theme_stylebox_override(&"panel", get_theme_stylebox(&"panel", &"Panel"))
 	get_menu_hbox().size_flags_horizontal = Control.SIZE_EXPAND_FILL
 	var line_edit := LineEdit.new()
 	line_edit.custom_minimum_size.x = 480
 	line_edit.size_flags_horizontal = Control.SIZE_EXPAND_FILL
 	line_edit.placeholder_text = "Root resource/directory here. (default=res://)"
-	line_edit.right_icon = interface.get_base_control().get_theme_icon(&"NewRoot", &"EditorIcons")
+	if interface: line_edit.right_icon = interface.get_base_control().get_theme_icon(&"NewRoot", &"EditorIcons")
 	get_menu_hbox().add_child(line_edit)
 	line_edit.text_submitted.connect(_line_submitted)
 
@@ -23,8 +22,8 @@ func _line_submitted(text: String) -> void:
 	for i in get_children():
 		if i in nodes.values():
 			i.queue_free()
-			nodes.erase(nodes.find_key(i))
-	(await _add_file(text)).set_slot_enabled_left(0, false)
+	nodes.clear()
+	(await _add_file(text))
 
 
 func _add_file(file: String) -> GraphNode:
@@ -41,21 +40,27 @@ func _add_file(file: String) -> GraphNode:
 		await inst.resized
 
 		var dependencies := ResourceLoader.get_dependencies(file)
-		var _prev_n: Array[GraphNode] = []
+		var _last_n: GraphNode
 		for i in dependencies.size():
 			var path = dependencies[i].get_slice("::", 0)
 			var dep_label := Label.new()
-			dep_label.text = str(i)
+			dep_label.text = str(i + 1)
 			dep_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_RIGHT
 			inst.add_child(dep_label)
 			inst.set_slot_enabled_right(i + 1, true)
 			var node = nodes[path] if nodes.has(path) else await _add_file(path)
-			node.set(&"position_offset", inst.position_offset + Vector2(inst.size.x + snapping_distance * 3, (_prev_n[-1].size.y + snapping_distance * 3 if !_prev_n.is_empty() else 0)))
-			_prev_n.append(node)
-			print(i)
 			connect_node(inst.name, i, node.name, 0)
+			await get_tree().process_frame
+			node.set(&"position_offset", inst.position_offset + Vector2(inst.size.x + snapping_distance * 8, (_last_n.position_offset.y + _last_n.size.y + snapping_distance * 3 if _last_n else -dependencies.size() * 78. / 2.)))
+			_last_n = node
 	else:
 		inst.title = "Invalid! (%s)" % file.get_file()
 		inst.set_slot_color_left(0, Color.RED)
 		(inst.get_node(^"%Type") as Label).text = "Possible cyclic dependency, or incorrect path."
 	return inst
+
+
+func _on_child_entered_tree(node: Node) -> void:
+	if node is GraphElement:
+		await get_tree().process_frame
+		node.reset_size()
